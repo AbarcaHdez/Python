34d5e57f12db2f3376e2e9ef7a5d20ea83440bdb
Juan Abarca <abarcahdez.juan@gmail.com>
2026-06-04 17:36:54 -0600

    Proyecto CRUD final sqlite

--- services/usuarios.py
+++ services/usuarios.py
@@ -1,34 +1,16 @@
-import json
-import os
 import sqlite3
 
 class UsuarioService:
-    
+
     def __init__(self):
-        self.conn = sqlite3.connect("data/usuarios.db", check_same_thread=False)
+        self.conn = sqlite3.connect(
+            "data/usuarios.db",
+            check_same_thread=False
+        )
         self.cursor = self.conn.cursor()
         self.crear_tabla()
-        self.ruta = "data/usuarios.json"
-        
 
-    #---------------- Funciones y CRUD ----------------#
-    
-    def cargar_usuarios(self):
-        if os.path.exists(self.ruta):
-            with open(self.ruta, "r") as f:
-                self.usuarios = json.load(f)
-        else:
-            self.usuarios = []
-
-        # calcular siguiente ID
-        if self.usuarios:
-            self.id = max(u["id"] for u in self.usuarios) + 1
-        else:
-            self.id = 1
-
-    def guardar_usuarios(self):
-        with open(self.ruta, "w") as f:
-            json.dump(self.usuarios, f, indent=4)
+    # ---------------- Funciones y CRUD ---------------- #
 
     def agregar_usuario(self, nombre, edad, usuario):
         try:
@@ -47,7 +29,10 @@ class UsuarioService:
         return [self.formatear_usuario(f) for f in filas]
 
     def obtener_usuarioById(self, id):
-        self.cursor.execute("SELECT * FROM usuarios WHERE id = ?", (id,))
+        self.cursor.execute(
+            "SELECT * FROM usuarios WHERE id = ?",
+            (id,)
+        )
         fila = self.cursor.fetchone()
         return self.formatear_usuario(fila)
 
@@ -60,52 +45,58 @@ class UsuarioService:
         return self.cursor.rowcount > 0
 
     def eliminar_usuario(self, id):
-        self.cursor.execute("DELETE FROM usuarios WHERE id = ?", (id,))
+        self.cursor.execute(
+            "DELETE FROM usuarios WHERE id = ?",
+            (id,)
+        )
         self.conn.commit()
         return self.cursor.rowcount > 0
-    
-    #---------------- Validaciones Backend ----------------#
+
+    # ---------------- Validaciones Backend ---------------- #
 
     def existe_usuario(self, usuario):
-        self.cursor.execute("SELECT 1 FROM usuarios WHERE usuario = ?", (usuario,))
+        self.cursor.execute(
+            "SELECT 1 FROM usuarios WHERE usuario = ?",
+            (usuario,)
+        )
         return self.cursor.fetchone() is not None
-    
+
     def existe_usuario_en_edicion(self, usuario, id):
         self.cursor.execute(
             "SELECT 1 FROM usuarios WHERE usuario = ? AND id != ?",
             (usuario, id)
         )
         return self.cursor.fetchone() is not None
-    
+
     def validar_nombre(self, nombre):
         if not isinstance(nombre, str):
             return "Nombre debe ser texto"
-        
+
         if nombre.strip() == "":
             return "Nombre vacío"
-        
+
         return None
-    
+
     def validar_usuario(self, usuario):
         if not isinstance(usuario, str):
             return "Usuario debe ser texto"
-        
+
         if usuario.strip() == "":
             return "Usuario vacío"
-        
+
         return None
-    
+
     def validar_edad(self, edad):
         if not isinstance(edad, int):
             return "Edad debe ser número"
-        
+
         if edad < 0:
             return "Edad inválida"
-        
+
         return None
-    
-    #---------------- Base de Datos ----------------#
-    
+
+    # ---------------- Base de Datos ---------------- #
+
     def crear_tabla(self):
         self.cursor.execute("""
             CREATE TABLE IF NOT EXISTS usuarios (
@@ -116,7 +107,7 @@ class UsuarioService:
             )
         """)
         self.conn.commit()
-        
+
     def formatear_usuario(self, fila):
         if not fila:
             return None
